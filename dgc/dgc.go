5e1d268113166d0519832a7b49e9bb53f0fd847b
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-07-01 18:22:06 -0400

    Create skeleton app

---
+++ dgc/dgc.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"github.com/codegangsta/cli"
+)
+
+func runDgc(c *cli.Context) {
+}
+
+func main() {
+	app := cli.NewApp()
+	dgc.Name = "dgc"
+	dgc.Usage = "A minimal docker garbage collector"
+	dgc.Version = "0.1.0"
+	dgc.Author = "David J Felix <davidjfelix@davidjfelix.com>"
+	dgc.Action = runDgc
+	app.Flags = []cli.Flag {
+	}
+	app.Run(os.Args)
+}
