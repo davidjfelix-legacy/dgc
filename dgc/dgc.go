008ccaf4f5ea94e1ee3a17419ad209b883d55fdd
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-07-03 22:44:01 -0400

    Create command line args to simulate old script

--- dgc/dgc.go
+++ dgc/dgc.go
@@ -19,6 +19,30 @@ func main() {
 	dgc.Author = "David J Felix <davidjfelix@davidjfelix.com>"
 	dgc.Action = runDgc
 	app.Flags = []cli.Flag {
+		cli.StringFlag {
+			Name: "grace, g",
+			Value: "3600",
+			Usage: "the grace period for a container, defualt time unit is seconds",
+			EnvVar: "GRACE_PERIOD_SECONDS,GRACE_PERIOD",
+		},
+		cli.StringFlag {
+			Name: "time-unit, t",
+			Value: "s",
+			Usage: "the time unit used for the grace period",
+			EnvVar: "GRACE_PERIOD_TIME_UNIT,TIME_UNIT",
+		},
+		cli.StringFlag {
+			Name: "docker, d",
+			Value: "docker",
+			Usage: "the docker executable",
+			EnvVar: "DOCKER",
+		},
+		cli.StringFlag {
+			Name: "exclude, e",
+			Value: "/etc/docker-gc-exclude",
+			Usage: "the directory of the list of containers to exclude from garbage collection",
+			EnvVar: "EXCLUDE_FROM_GC",
+		}
 	}
 	app.Run(os.Args)
 }
