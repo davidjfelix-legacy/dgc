f04b972277a444fab5df8d9cfdc0e8365ad688e3
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-07-21 14:23:27 -0400

    Create new skeleton and add args

--- dgc/dgc.go
+++ dgc/dgc.go
@@ -2,42 +2,45 @@ package main
 
 import (
 	"fmt"
+	"errors"
 	"os"
+	"time"
 	"github.com/codegangsta/cli"
 	"github.com/fsouza/go-dockerclient"
 )
 
-func runDgc(c *cli.Context) {
-	client, _ := docker.NewClient(c.String("socket"))
+func collectAPIImages(images []APIImages, client *Client, ctx *cli.Context) (error) {
+}
+
+func handleImage(image *Image, client *Client, grace *time.Duration, bool output) (error) {
+}
+
+func collectAPIContainers(containers []APIContainers, client *Client, ctx *cli.Context) (error) {
+}
+
+func handleContainer(container *Container, client *Client, grace *time.Duration, bool output) (error) {
+}
+
+func runDgc(ctx *cli.Context) {
+	client, _ := docker.NewClient(ctx.String("socket"))
 	images, _ := client.ListImages(docker.ListImagesOptions{All: false})
 	containers, _ := client.ListContainers(docker.ListContainersOptions{All: false})
-	for _, image := range images {
-		fmt.Println("ID: ", image.ID)
-		fmt.Println("RepoTags: ", image.RepoTags)
-		fmt.Println("Created: ", image.Created)
-		fmt.Println("Size: ", image.Size)
-		fmt.Println("VirtualSize: ", image.VirtualSize)
-		fmt.Println("ParentId: ", image.ParentID)
-	}
-
-	for _, container := range containers {
-		fmt.Println("ID: ", container.ID)
-		fmt.Println("Image: ", container.Image)
-		fmt.Println("Command: ", container.Command)
-	}
+	collectAPIContainers(containers, &client, ctx)
+	collectAPIImages(containers, &client, ctx)
 }
 
 func main() {
 	dgc := cli.NewApp()
+	dgc.EnableBashCompletion = true
 	dgc.Name = "dgc"
 	dgc.Usage = "A minimal docker garbage collector"
 	dgc.Version = "0.1.0"
 	dgc.Author = "David J Felix <davidjfelix@davidjfelix.com>"
 	dgc.Action = runDgc
 	dgc.Flags = []cli.Flag {
-		cli.StringFlag {
+		cli.DurationFlag {
 			Name: "grace, g",
-			Value: "3600s",
+			Value: time.Duration(3600)*time.Second,
 			Usage: "the grace period for a container. Accepted compostable time units: [h, m, s, ms, ns us]",
 			EnvVar: "GRACE_PERIOD_SECONDS,GRACE_PERIOD",
 		},
@@ -52,6 +55,11 @@ func main() {
 			Value: "/etc/docker-gc-exclude",
 			Usage: "the list of containers to exclude from garbage collection, as a file or directory",
 			EnvVar: "EXCLUDE_FROM_GC",
+		},
+		cli.BoolFlag {
+			Name: "verbose, v",
+			Usage: "print name of garbage-collected containers",
+			EnvVar: "VERBOSE",
 		},
 	}
 	dgc.Run(os.Args)
